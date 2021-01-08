575732935316614c6136021ad1b265f19fafdb16
Ryan Tay <tay.yee.en@gmail.com>
2021-01-09 05:33:59 +0800

    mutiple file upload

--- file-upload-react/src/App.js
+++ file-upload-react/src/App.js
@@ -1,48 +1,19 @@
-import React, { Component } from 'react'
-import './App.css'
-import axios from 'axios'
+import React from "react";
+import "./App.css";
 
-const endpoint = 'http://localhost:3000/upload'
+import UploadFiles from "./components/upload-file.component";
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      selectedFile: null,
-      loaded: 0,
-    }
-  }
-  handleselectedFile = event => {
-    this.setState({
-      selectedFile: event.target.files[0],
-      loaded: 0,
-    })
-  }
-  handleUpload = () => {
-    const data = new FormData()
-    data.append('file', this.state.selectedFile, this.state.selectedFile.name)
-
-    axios
-      .post(endpoint, data, {
-        onUploadProgress: ProgressEvent => {
-          this.setState({
-            loaded: (ProgressEvent.loaded / ProgressEvent.total) * 100,
-          })
-        },
-      })
-      .then(res => {
-        console.log(res.statusText)
-      })
-  }
-  render() {
-    return (
-      <div className="App">
-        <input type="file" name="" id="" onChange={this.handleselectedFile} />
-        <button onClick={this.handleUpload}>Upload</button>
-        <div> {Math.round(this.state.loaded, 2)} %</div>
+function App() {
+  return (
+    <div className="container" style={{ width: "600px" }}>
+      <div>
+        <h3>bezkoder.com</h3>
+        <h4>React upload multiple Files</h4>
       </div>
-    )
-  }
+
+      <UploadFiles />
+    </div>
+  );
 }
 
-export default App
+export default App;
